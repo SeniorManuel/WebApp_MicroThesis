02ba6f5256f5d42bb88ee97e64b3a540e5da014c
SeniorManuel <dominiaflores853@gmail.com>
2025-09-16 00:13:45 +0800

    7th changes

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'Frontend',  
-
+  root: '.', 
   plugins: [react()],
-
   build: {
-    outDir: 'Frontend/dist', 
-    emptyOutDir: true, 
-  },
-
-  resolve: {
-    alias: {
-      '@': '/Frontend/src', 
-    },
+    outDir: 'dist', 
   },
 })
