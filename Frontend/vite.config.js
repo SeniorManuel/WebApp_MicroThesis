01512102350be046825e8fbafd202bda26003014
SeniorManuel <dominiaflores853@gmail.com>
2025-09-16 00:05:50 +0800

    6th new root

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   root: 'Frontend',  
+
   plugins: [react()],
+
   build: {
     outDir: 'Frontend/dist', 
+    emptyOutDir: true, 
+  },
+
+  resolve: {
+    alias: {
+      '@': '/Frontend/src', 
+    },
   },
 })
